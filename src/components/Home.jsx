323ec95a5a14bf53c644e444532de0cd8692c6a6
Modou Tamba <tambamodou68@gmail.com>
2023-10-31 04:06:57 -0700

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a FrontEnd Developer
+            I'm a Junior Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I am a Frontend developer who can take up Full Stack projects in both Web and Mobile Applications and able to sucessfully develop softwares base on clients request.
+            I am a Junior developer who can take up Full Stack projects in both Web and Mobile Applications and able to sucessfully develop softwares base on clients request.
             Currently, I love to work on web application using technologies like
-            React, Tailwind, BootStrap, NodeJs, Laravel, and Flutter for mobile app development etc.
+            React, Tailwind, BootStrap, NodeJs, Laravel, and React Native for mobile app development etc.
           </p>
           <div>
             <Link
